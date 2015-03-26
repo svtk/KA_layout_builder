cdad1e89b5e764f90b23a83cb577e2a5d87b6fb2
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2015-03-26 21:29:50 +0100

    Created project; configured kotlin

---
+++ app/src/main/java/com/svtk/ka_layout_builder/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.svtk.ka_layout_builder
+
+import android.support.v7.app.ActionBarActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+
+
+public class MainActivity : ActionBarActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+}
